cfe84184e31025ea8c19fd2e4cd21ff1741b03f9
PNRRUS <pomaikai7378@gmail.com>
2024-01-19 23:09:37 +0300

    commitRoma^_^

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,8 @@
 public class Main {
     public static void main(String[] args) {
+        boolean Roma = true;
         System.out.println("Hello world!");
-
+System.out.println("Roma was here " + Roma );
 //        Изменения первого коммита
 //        Изменения первой ветки
 //        Изменения ветки номер 2
